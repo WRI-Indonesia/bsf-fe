d7eb38e3fa6902a7deafd9f0307bc5c5eb952d61
Aulia Rifkyano <aulia.rifkyano@wri.org>
2026-04-20 12:15:13 +0700

    Add flag remote pattern

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
