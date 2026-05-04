7cc3566c3c9506a75d353471e76ff71e4ada147c
Aulia Rifkyano <aulia.rifkyano@wri.org>
2026-05-04 09:58:07 +0700

    Fix migration file

--- src/migrations/20260504_024628.ts
+++ src/migrations/20260504_024628.ts
@@ -2,16 +2,14 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   CREATE TYPE "public"."enum_users_role" AS ENUM('admin', 'editor');
-  ALTER TABLE "events_key_dates" ADD COLUMN "show" boolean DEFAULT true;
-  ALTER TABLE "users" ADD COLUMN "name" varchar NOT NULL;
-  ALTER TABLE "users" ADD COLUMN "role" "enum_users_role" DEFAULT 'editor' NOT NULL;`)
+   CREATE TYPE IF NOT EXISTS "public"."enum_users_role" AS ENUM('admin', 'editor');
+  ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "name" varchar NOT NULL;
+  ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "role" "enum_users_role" DEFAULT 'editor' NOT NULL;`)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "users" DROP COLUMN "name";
-  ALTER TABLE "users" DROP COLUMN "role";
-  ALTER TABLE "events_key_dates" DROP COLUMN "show";
-  DROP TYPE "public"."enum_users_role";`)
+   ALTER TABLE "users" DROP COLUMN IF EXISTS "name";
+  ALTER TABLE "users" DROP COLUMN IF EXISTS "role";
+  DROP TYPE IF EXISTS "public"."enum_users_role";`)
 }
