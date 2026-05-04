3045a4f1dd0e4630ea6e94dc4ab8e212a92505cd
Aulia Rifkyano <aulia.rifkyano@wri.org>
2026-05-04 10:02:14 +0700

    Add checker for older postgresql version

--- src/migrations/20260504_024628.ts
+++ src/migrations/20260504_024628.ts
@@ -2,9 +2,23 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   CREATE TYPE IF NOT EXISTS "public"."enum_users_role" AS ENUM('admin', 'editor');
-  ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "name" varchar NOT NULL;
-  ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "role" "enum_users_role" DEFAULT 'editor' NOT NULL;`)
+   DO $$ BEGIN
+    CREATE TYPE "public"."enum_users_role" AS ENUM('admin', 'editor');
+   EXCEPTION
+    WHEN duplicate_object THEN null;
+   END $$;
+
+   DO $$ BEGIN
+    ALTER TABLE "users" ADD COLUMN "name" varchar NOT NULL;
+   EXCEPTION
+    WHEN duplicate_column THEN null;
+   END $$;
+
+   DO $$ BEGIN
+    ALTER TABLE "users" ADD COLUMN "role" "public"."enum_users_role" DEFAULT 'editor' NOT NULL;
+   EXCEPTION
+    WHEN duplicate_column THEN null;
+   END $$;`)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
